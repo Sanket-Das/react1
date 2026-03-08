600eb487453a6f01390920a599faecf6647fbac1
Ankur-006 <ankurmalakar712@gmail.com>
2026-03-08 12:29:01 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
+import Header from './components/Header'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
   const [count, setCount] = useState(0)
 
   return (
-    <Blogs/>
-    // <Home />
-    // <Header/>
+   <Header/>
   )
 }
 
